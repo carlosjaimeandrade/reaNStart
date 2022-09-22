09a43ded681910b1a0527f194d8171182c752d97
Carlos Jaime <jaime_andrek@hotmail.com>
2022-09-22 20:50:29 -0300

    Criando botão

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native'
-import {View, SafeAreaView, Text} from 'react-native'
+import {View, SafeAreaView, Text, Button, TouchableNativeFeedback } from 'react-native'
 
 
 const Page = styled.SafeAreaView`
-  background-Color: red;
+  background-Color: #fff;
   height: 100%;
+`;
+
+const Btn = styled.View`
+  background-color: blue;
+  border-radius: 3px;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  border-radius: 50px;
+  border: 1px solid black
+  margin-top: 10px
+  font-size: 32px
+  text-align: center
+  background-color: red;
+  }
+`;
+
+
+
+const Input = styled.TextInput`
+  width: 100%;
+  border: 1px solid;
+  padding: 20px;
+  background-color: #eee;
+  color: black;
+  font-size: 30px;
 `;
 
 const Header = styled.View`
@@ -24,9 +50,59 @@ const Quadrado = styled.View`
   margin: 15px;
 `;
 
+const P = styled.Text`
+  font-size: 30px;
+`;
+
+const Label = styled.Text`
+  font-size: ${props=>props.size}px;
+  text-align: center;
+  color: #fff;
+`;
+
+
+
+const Hello = (props) => {
+
+  const [name, setName] = useState('Bonieky');
+  
+  return (
+    <P>{props.frase} {name}</P>
+  )
+}
+
 export default () => {
+
+  const [nome, setNome] = useState('Carlos');
+  const [idade, setIdade] = useState('52');
+
+  const mudaNome = (nome) =>{
+    setNome(nome)
+  }
+
+  const mudaIdade = (idade) =>{
+    setIdade(idade)
+  }
+
   return (
     <Page>
+      <Hello frase="Seja bem vindo"></Hello>
+
+      <P>Nome: {nome}</P>
+      <Input value={nome} onChangeText={mudaNome} placeholder="useless placeholder"  keyboardType="text"></Input>
+
+      <P>Idade: {idade}</P>
+      <Input value={idade} onChangeText={mudaIdade} placeholder="Insira a idade" keyboardType="numeric"  ></Input>
+
+  
+      <TouchableNativeFeedback>
+          <Btn onPress={() => alert('cool')} ><Label size="25">Entrar</Label></Btn>
+      </TouchableNativeFeedback>
+
+      <Button title="Cancelar"></Button>
+
+
+
       <Header>
         <Quadrado cor="red"></Quadrado>
         <Quadrado cor="blue"></Quadrado>
