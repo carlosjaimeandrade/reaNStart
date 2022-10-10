0fc3e34115cdc70871bb1b6f052d609180e19fc3
Carlos Jaime <jaime_andrek@hotmail.com>
2022-10-10 20:24:40 -0300

    adicionando botão no header

--- src/navigators/MainStack.js
+++ src/navigators/MainStack.js
@@ -2,6 +2,7 @@ import React from  'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../pages/HomeScreen';
 import AboutScreen from '../pages/AboutScreen';
+import { Button } from 'react-native';
 
 const MainStack = createStackNavigator();
 
@@ -17,7 +18,11 @@ export default () =>{
                 color: '#fff'
             }
         }}>
-            <MainStack.Screen name="Home" component={HomeScreen} />
+            <MainStack.Screen name="Home" component={HomeScreen} options={{
+                headerLeft:  () => <Button title="Opa1" onPress={ ()=> alert('ola mundo2')} ></Button>,
+                headerRight: () => <Button title="Opa1" onPress={ ()=> alert('ola mundo1')} ></Button>,
+                headerTitle: () => <Button title="Opa2" onPress={ ()=> alert('ola mundo2')} ></Button>
+            }} />
             <MainStack.Screen name="About" component={AboutScreen} />
         </MainStack.Navigator>
     )
