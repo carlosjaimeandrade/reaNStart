ac2c8f1c49afb5f192410eaae6e7f6eb739af8c1
Carlos Jaime <jaime_andrek@hotmail.com>
2022-10-25 20:48:16 -0300

    modificando o tab navigation padrão

--- src/navigators/MainTabs.js
+++ src/navigators/MainTabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 
 import HomeScreen from '../pages/HomeScreen';
 import AboutScreen from '../pages/AboutScreen';
@@ -7,8 +8,39 @@ import AboutScreen from '../pages/AboutScreen';
 const Tab = createBottomTabNavigator();
 
 export default () => (
-    <Tab.Navigator>
-        <Tab.Screen name="TabHome"  component={HomeScreen} />
-        <Tab.Screen name="TabAbout" component={AboutScreen} />
+    <Tab.Navigator
+        tabBarOptions={{ 
+            showIcon: true, 
+            showLabel: true,
+            activeTintColor: '#fff',
+            activeBackgroundColor: 'blue',
+            style:{
+                height: 120
+            },
+            labelStyle:{
+                fontSize: 18
+            },
+            labelPosition: 'beside-icon',
+            keyboardHidesTabBar: true
+        }}
+        screenOptions={({route})=>({
+            tabBarIcon: (focused, color, size) => {
+                let imgSource = null;
+               
+                switch(route.name){
+                    case 'TabHome': 
+                        imgSource = require('../assets/home.png')
+                        break
+                    case 'TabAbout':
+                        imgSource = require('../assets/interface.png')
+                } 
+
+
+                return <Image source={imgSource} style={{width: 24, height: 24}} />
+            }
+        })}
+    >
+        <Tab.Screen name="TabHome"  component={HomeScreen}  options={{tabBarLabel:"home"}} />
+        <Tab.Screen name="TabAbout" component={AboutScreen} options={{tabBarLabel:"About"}}/>
     </Tab.Navigator>
 );
